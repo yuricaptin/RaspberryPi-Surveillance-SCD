286be707310b95673e9e4fd11a34874fe39306bb
Lamar York <61609037+yuricaptin@users.noreply.github.com>
2023-04-05 11:06:47 -0700

    Included code from RPEvaluation, code change

--- RPTraining.py
+++ RPTraining.py
@@ -52,16 +52,20 @@ def train(data, EPOCHS):
 
     for epoch in range(1, EPOCHS+1):
         print('\n Epoch {}/{}'.format(epoch, EPOCHS))
-        progbar = tf.keras.utils.Progbar(len(train_data))
+        progbar = tf.keras.utils.Progbar(len(data))
 
         # Creating a metric object
         r = Recall()
         p = Precision()
 
-        for idx, batch in enumerate(train_data):
+        for idx, batch in enumerate(data):
 
-            train_step(batch)
+            loss = train_step(batch)
+            yhat = siamese_model.predict(batch[:2])
+            r.update_state(batch[2], yhat)
+            p.update_state(batch[2], yhat)
             progbar.update(idx+1)
+        print(loss.numpy(), r.result().numpy(), p.result().numpy())
 
         if epoch % 10 == 0:
             checkpoint.save(file_prefix=checkpoint_prefix)
@@ -69,3 +73,52 @@ def train(data, EPOCHS):
 EPOCHS = 50
 
 train(train_data, EPOCHS)
+
+test_input, test_val, y_true = test_data.as_numpy_iterator().next()
+
+y_hat = siamese_model.predict([test_input, test_val])
+
+[1 if prediction > 0.5 else 0 for prediction in y_hat]
+
+print(y_true)
+
+#Metrics calculation!
+m = Recall()
+m.update_state(y_true, y_hat)
+m.result().numpy()
+
+
+#Creation of the metric object
+m = Precision()
+m.update_state(y_true, y_hat)
+m.result().numpy()
+
+r = Recall()
+p = Precision()
+
+for test_input, test_val, y_true in test_data.as_numpy_iterator():
+    yhat = siamese_model.predict([test_input, test_val])
+    r.update_state(y_true, yhat)
+    p.update_state(y_true, yhat)
+
+print(r.result().numpy(), p.result().numpy())
+
+plt.figure(figsize=(10,8))
+
+plt.subplot(1,2,1)
+plt.imshow(test_input[0])
+
+plt.subplot(1,2,2)
+plt.imshow(test_val[0])
+
+plt.show()
+
+#Saving the weights (Update the weights also works)
+siamese_model.save('siamesemodelv2.h5')
+L1Dist
+
+#Reloading of the siamese model
+siamese_model = tf.keras.models.load_model('siamesemodelv2.h5', custom_objects = {'L1Dist':L1Dist, 'BinaryCrossentropy':tf.losses.BinaryCrossentropy})
+
+#Viewing the model summary
+siamese_model.summary()
